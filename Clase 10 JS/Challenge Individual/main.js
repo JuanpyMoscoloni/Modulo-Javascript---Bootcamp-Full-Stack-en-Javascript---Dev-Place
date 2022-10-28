ba4fcc9916f249d6b09375d6987ec057827ac37b
JuanpyMoscoloni <moscolonijuanpablo@gmail.com>
2022-10-28 13:06:54 -0300

    Challenge

---
+++ Clase 10 JS/Challenge Individual/main.js
@@ -0,0 +1,144 @@
+/*Se pretende realizar una aplicación para esta academia que gestione la información sobre las personas vinculadas con la misma, que se pueden clasificar en tres tipos: estudiantes, profesores y personal de servicio. A continuación, se detalla qué tipo de información debe gestionar esta aplicación:
+• Por cada persona, se debe conocer, al menos, su nombre y apellidos, su número de identificación y su estado civil.
+• Con respecto a los empleados, sean del tipo que sean, hay que saber su año de incorporación a la facultad y qué número de despacho tienen asignado.
+• En cuanto a los estudiantes, se requiere almacenar el curso en el que están matriculados.
+• Por lo que se refiere a los profesores, es necesario gestionar a qué departamento pertenecen (base de datos, java, php, ...).
+• Sobre el personal de servicio, hay que conocer a qué sección están asignados (biblioteca, decanato, secretaría, ...).
+El ejercicio consiste, en primer lugar, en definir la jerarquía de clases de esta aplicación. A continuación, debe programar las clases definidas en las que, además de los constructores, hay que desarrollar los métodos correspondientes a las siguientes acciones:
+• Cambio del estado civil de una persona.
+• Reasignación de despacho a un empleado.
+• Matriculación de un estudiante en un nuevo curso.
+• Cambio de departamento de un profesor.
+• Traslado de sección de un empleado del personal de servicio.
+• Imprimir toda la información de cada tipo de individuo.*/
+
+
+class Persona{
+    nombre;
+    apellido;
+    id;
+    estadocivil;
+
+    constructor(nombre,apellido,id,estadocivil){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.id = id;
+        this.estadocivil = estadocivil;
+    }
+
+    cambioEstadoCivil(estado){
+        this.estadocivil=estado;
+    }
+}
+
+class Empleados extends Persona{
+    aIncorporacion;
+    nDespacho;
+
+    constructor(nombre,apellido,id,estadocivil,aIncorporacion,nDespacho){
+        super(nombre,apellido,id,estadocivil);
+        this.aIncorporacion = aIncorporacion;
+        this.nDespacho = nDespacho;
+    }
+
+    reasignacionDespacho(newDespacho){
+        this.nDespacho = newDespacho;
+    }
+}
+
+class Estudiantes extends Persona{
+    clase;
+    
+    constructor(nombre,apellido,id,estadocivil,clase){
+        super(nombre,apellido,id,estadocivil,clase);
+        this.clase = clase;
+    }
+
+    nuevaClase(newClase){
+        this.clase = newClase;
+    }
+}
+
+class Profesores extends Empleados {
+    departamento;
+
+    constructor(nombre,apellido,id,estadocivil,aIncorporacion,nDespacho,departamento){
+        super(nombre,apellido,id,estadocivil,aIncorporacion,nDespacho,departamento);
+        this.departamento = departamento;
+    }
+
+    nuevoDepartamento(newDepartamento){
+        this.departamento = newDepartamento;
+    }
+}
+
+class PersonalServicio extends Empleados {
+    seccion;
+
+    constructor(nombre,apellido,id,estadocivil,aIncorporacion,nDespacho,seccion){
+        super(nombre,apellido,id,estadocivil,aIncorporacion,nDespacho,seccion);
+        this.seccion = seccion;
+    }
+
+    nuevaSeccion(newSeccion){
+        this.seccion = newSeccion;
+    }
+}
+
+// class Curso{
+//     listaEstudiantes;
+//     grado;
+//     constructor(grado){
+//         this.listaEstudiantes = [];
+//         this.grado = grado;
+//     }
+
+//     nuevoEstudiante(estudiante){
+//         this.listaEstudiantes.push(estudiante); 
+//         let indiceContacto = 0;
+//         indiceContacto=this.listaEstudiantes.indexOf(estudiante);
+//         this.listaEstudiantes[indiceContacto].clase=this.grado;
+//     }
+
+//     eliminarEstudiante(estudiante){
+//         let indiceContacto = 0;
+//         indiceContacto=this.listaEstudiantes.indexOf(estudiante);
+//         listaEstudiantes.splice(indiceContacto,1);
+//     }
+// }
+
+let lista = [];
+
+let estudiante = new Estudiantes('Juanpy', 'Moscoloni', '44335612','Soltero','3');
+let estudiante1 = new Estudiantes('Lucas', 'Pereyra', '44271923','Casado','5');
+estudiante1.nuevaClase('2');
+lista.push(estudiante,estudiante1);
+console.table(lista);
+
+let listaProfesor = [];
+
+let profesor = new Profesores('Jazmin', 'Jirafales', '20123422','Casado','2020','2','5');
+profesor.nuevoDepartamento('2');
+listaProfesor.push(profesor);
+console.table(listaProfesor);
+
+
+// let cursito = new Curso('7');
+// let cursito1 = new Curso('20');
+// cursito1.nuevoEstudiante(estudiante1);
+// console.table(cursito1.listaEstudiantes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Clase 9 JS/Challenge Grupal/main.js
+++ Clase 9 JS/Challenge Grupal/main.js
@@ -1,85 +1,54 @@
-const prompt = require("prompt-sync")({ sigint: true });
-// class Persona {
-//   constructor() {
-//     this.dni = dni;
-//     this.nombre = nombre;
-//     this.apellido = apellido;
-//     this.telefono = telefono;
-//   }
-// }
+function datos(){
+  class Persona{
+      nombre;
+      edad;
+      dni;
+      telefono;
+      pais;
 
-var agenda = [
-  {
-    dni: 33213221,
-    nombre: "Marcelo",
-    apellido: "Albano",
-    telefono: 2236771829,
-  },
-  {
-    dni: 44335612,
-    nombre: "Juan Pablo",
-    apellido: "Moscoloni",
-    telefono: 2235882716,
-  },
-];
-
-let decision;
-
-do {
-  decision = prompt(
-    "Ingrese una Opción: \n1. Agregar un Contacto\n2. Eliminar por DNI\n3. Mostrar por DNI\n4. Mostrar toda la agenda \n0. Salir\n\n"
-  );
-  switch (decision) {
-    case "1":
-      agregarUser(agenda);
-      break;
-
-    case "2":
-      let eliminarDNI = prompt(
-        "Ingrese el DNI del Contacto que quiera ELIMINAR: "
-      );
-      eliminarContacto(eliminarDNI);
-      break;
-
-    case "3":
-      let buscarDNI = prompt(
-        "Ingrese el DNI del Contacto que quiera ENCONTRAR: "
-      );
-      verPersona(buscarDNI);
-      break;
-
-    case "4":
-      mostrarAgenda(agenda);
-      break;
-
-    case "0":
-      console.log("Adios!\n");
-      break;
-
-    default:
-      console.log("El dato ingresado es incorrecto\n");
-      break;
+      constructor(nombre, edad, dni, telefono, pais){
+          this.nombre = nombre;
+          this.edad = edad;
+          this.dni = dni;
+          this.telefono = telefono;
+          this.pais = pais;
+      }
   }
-} while (decision != "0");
 
-function agregarUser(agenda) {
-  //   let usuarios = [];
-  let dni = parseInt(prompt("Ingresar DNI: "));
-  let nombre = prompt("Ingresar Nombre: ");
-  let apellido = prompt("Ingresar Apellido: ");
-  let telefono = parseInt(prompt("Ingresar Telefono: "));
-  let usuarios = { dni, nombre, apellido, telefono };
-  agenda.push(usuarios);
+  var nombreObtener = document.getElementById("nombre").value;
+  var edadObtener = document.getElementById("edad").value;
+  var dniObtener = document.getElementById("dni").value;
+  var telefonoObtener = document.getElementById("telefono").value;
+  var paisObtener = document.getElementById("pais").value;
+  
+  personaNueva = new Persona(nombreObtener, edadObtener, dniObtener, telefonoObtener, paisObtener);
+
+  agregarDatos();
+  updateVegetablesCollection();
+
 }
 
-function eliminarContacto(dni) {
-  let eleccionContacto = agenda.find((elemento) => elemento.dni === dni);
-  let indiceContacto = agenda.indexOf(eleccionContacto);
-  agenda.splice(indiceContacto, 1);
+var datosTotal = [];
+
+function agregarDatos(){
+  datosTotal.push(personaNueva);
+  console.log(datosTotal);
+  document.getElementById("tabla").innerHTML += '<td>'+ personaNueva.nombre +'<td>'+ personaNueva.edad +'<td>'+ personaNueva.telefono +'<td>'+ personaNueva.dni + '<td>' + personaNueva.pais + '</td>';
+}
+
+function eliminarDatosssss(){
+  let eleccionContacto = datosTotal.find((elemento) => elemento.dni === dni);
+  let indiceContacto = datosTotal.indexOf(eleccionContacto);
+  datosTotal.splice(indiceContacto, 1);
+}
+
+function eliminarDatos() {
+  let eleccionContacto = datosTotal.find((elemento) => elemento.dni === dni);
+  let indiceContacto= datosTotal.indexOf(eleccionContacto); 
+    datosTotal.splice(indiceContacto, 1);
 }
 
 function verPersona(dni) {
-  //   let eleccionContacto = agenda.find((elemento) => elemento.dni === dni);
   let indiceContacto = 0;
   agenda.forEach((element) => {
     if (element.dni === parseInt(dni)) {
@@ -87,12 +56,4 @@ function verPersona(dni) {
     }
   });
   console.log(agenda[indiceContacto]);
-}
-
-function mostrarAgenda(agenda) {
-  let string = "";
-  agenda.forEach((element, index) => {
-    string += "[" + index + "] " + element + "\n";
-  });
-  console.log(agenda);
 }
